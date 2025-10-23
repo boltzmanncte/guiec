0bd4b37e3dca806322abdf1ad4c4de5e0edc46d9
David Amo <david.amo-gonzalez@ext.ec.europa.eu>
2025-10-23 12:41:16 +0100

    feat: disabled button behavior

--- MainMaui_Refact/vfv/vfv/Converters/Converters.cs
+++ MainMaui_Refact/vfv/vfv/Converters/Converters.cs
@@ -194,3 +194,16 @@ public class SelectedFilesCountTextConverter : IValueConverter
         throw new NotImplementedException();
     }
 }
+
+public class HasSelectedFilesConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is int count && count > 0;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
